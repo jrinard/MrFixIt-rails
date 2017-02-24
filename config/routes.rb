5dcf75cf5ec04f856fdfdbf1620ce9d40ab2fef9
Joshua Rinard <jrinard@hotmail.com>
2017-02-24 08:24:04 -0800

    configures environments and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+
+### may need to associate jobs and workers
+
 Rails.application.routes.draw do
+  root 'landing#index'
   devise_for :workers
   devise_for :users
-  root 'landing#index'
 
   resources :jobs
   resources :workers
